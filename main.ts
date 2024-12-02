33656324bcfaf493162ee0d2575fd9c21810938d
Richard Baumann <richard.baumann@xitaso.com>
2024-12-02 09:03:59 +0100

    Add day2p1

--- main.ts
+++ main.ts
@@ -51,6 +51,48 @@ export function day1p2(): number {
   ).reduce((accumulator: number, value: number) => accumulator + value)
 }
 
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
+export function day2p1(): number {
+  const example_input: string = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+  const day2input = Deno.readTextFileSync("input/day2.txt");
+  return day2input.split(/\r?\n/).reduce<number>(
+    (accumulator, line) => {
+      if (line.length <= 0) {
+        return accumulator;
+      }
+      const numbers: number[] = line.split(/(\d+)\s*/).filter((n) => n !== "").map((n) => +n);
+      const sorted = Object.assign([], numbers).sort((a, b) => a - b);
+      const reverseSorted = Object.assign([], sorted).reverse();
+      if (!(arrayEquals(numbers, sorted) || arrayEquals(numbers, reverseSorted))){
+        return accumulator;
+      }
+      const zipped = numbers.reduce((merged: [number, number][], c, index) => {
+        if (index + 1 < numbers.length) {
+          merged.push([c, numbers[index+1]]);
+        }
+        return merged;
+      }, []);
+      return zipped.some((entry) =>
+        Math.abs(entry[1] - entry[0]) < 1 || Math.abs(entry[1] - entry[0]) > 3
+      ) ? accumulator : accumulator + 1;
+    }, 0);
+}
+
+export function day2p2(): number {
+
+}
+
 type Day = {
   part1: () => number;
   part2: () => number;
@@ -62,6 +104,10 @@ if (import.meta.main) {
     {
       part1: day1p1,
       part2: day1p2
+    },
+    {
+      part1: day2p1,
+      part2: day2p2,
     }
   ];
 
