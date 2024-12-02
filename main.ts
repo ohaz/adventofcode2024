f89380ebc5639d35e81a264bb78831df4c381f04
Richard Baumann <ohaz@ohaz.engineer>
2024-12-02 18:31:44 +0100

    Refactor and solve p2

--- main.ts
+++ main.ts
@@ -51,11 +51,16 @@ export function day1p2(): number {
   ).reduce((accumulator: number, value: number) => accumulator + value)
 }
 
-function arrayEquals(a, b) {
-    return Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index]);
+function isSafe(f: number[]): boolean {
+  const gradient = f.reduce((merged: number[], c, index) => {
+    if (index + 1 < f.length) {
+      merged.push(f[index+1] - c);
+    }
+    return merged;
+  }, []);
+  return (gradient.every((m) => m > 0)
+          || gradient.every((m) => m < 0))
+          && !gradient.some((m) => Math.abs(m) < 1 || Math.abs(m) > 3);
 }
 
 export function day2p1(): number {
@@ -72,25 +77,34 @@ export function day2p1(): number {
         return accumulator;
       }
       const numbers: number[] = line.split(/(\d+)\s*/).filter((n) => n !== "").map((n) => +n);
-      const sorted = Object.assign([], numbers).sort((a, b) => a - b);
-      const reverseSorted = Object.assign([], sorted).reverse();
-      if (!(arrayEquals(numbers, sorted) || arrayEquals(numbers, reverseSorted))){
-        return accumulator;
-      }
-      const zipped = numbers.reduce((merged: [number, number][], c, index) => {
-        if (index + 1 < numbers.length) {
-          merged.push([c, numbers[index+1]]);
-        }
-        return merged;
-      }, []);
-      return zipped.some((entry) =>
-        Math.abs(entry[1] - entry[0]) < 1 || Math.abs(entry[1] - entry[0]) > 3
-      ) ? accumulator : accumulator + 1;
+      return isSafe(numbers) ? accumulator + 1 : accumulator;
     }, 0);
 }
 
-export function day2p2(): number {
+function generateProblemDampener(f: number[]): number[][] {
+  const results = [f];
+  for (let i = 0; i < f.length; i++) {
+    results.push(f.toSpliced(i, 1));
+  }
+  return results;
+}
 
+export function day2p2(): number {
+  const example_input: string = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+  const day2input = Deno.readTextFileSync("input/day2.txt");
+  return day2input.split(/\r?\n/).reduce<number>(
+    (accumulator, line) => {
+      if (line.length <= 0) {
+        return accumulator;
+      }
+      const numbers: number[] = line.split(/(\d+)\s*/).filter((n) => n !== "").map((n) => +n);
+      return generateProblemDampener(numbers).some((entry) => isSafe(entry)) ? accumulator + 1 : accumulator;
+    }, 0);
 }
 
 type Day = {
