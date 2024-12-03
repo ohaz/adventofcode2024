f3f6b3e42f2b312de7ee7305a2cfe22ae3ba4733
Richard Baumann <ohaz@ohaz.engineer>
2024-12-03 09:32:01 +0100

    Improve function name

--- day3.ts
+++ day3.ts
@@ -1,6 +1,6 @@
 import { Day } from "./day.ts";
 
-function parse(text: string): number {
+function mul(text: string): number {
     const parsed = text.matchAll(/(mul\((\d+),(\d+)\))/gm)
     return parsed.reduce((accumulator, entry) => {
         accumulator += +entry[2] * +entry[3];
@@ -11,7 +11,7 @@ function parse(text: string): number {
 function part1(): number {
     const day3input = Deno.readTextFileSync("input/day3.txt");
     const _example_input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`;
-    return parse(day3input);
+    return mul(day3input);
 }
 
 type dodont = 'do()' | 'don\'t()';
@@ -33,7 +33,7 @@ function part2(): number {
         currentIndex = nextIndex > 0 ? nextIndex + nextCommand.length : input.length;
         currentType = nextCommand;
     }
-    return parse(dostring);
+    return mul(dostring);
 }
 
 export const day3: Day = {
