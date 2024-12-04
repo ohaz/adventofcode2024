da043c5226a45b9e00243061b22d83f8f572719e
Richard Baumann <ohaz@ohaz.engineer>
2024-12-04 18:45:05 +0100

    Format

--- days/day4.ts
+++ days/day4.ts
@@ -5,59 +5,81 @@ function split(text: string): string[][] {
   if (splitText[splitText.length - 1] == "") {
     splitText.pop();
   }
-  splitText.unshift('.'.repeat(splitText[0].length))
-  splitText.push('.'.repeat(splitText[0].length))
-  return splitText.map((line) => `.${line}.`).map((line) => line.split(''));
+  splitText.unshift(".".repeat(splitText[0].length));
+  splitText.push(".".repeat(splitText[0].length));
+  return splitText.map((line) => `.${line}.`).map((line) => line.split(""));
 }
 
 type Coordinate = {
-  row: number,
-  col: number,
-}
+  row: number;
+  col: number;
+};
 
-function add_coordinates(coordinate1: Coordinate, coordinate2: Coordinate): Coordinate {
-  return {row: coordinate1.row + coordinate2.row, col: coordinate1.col+coordinate2.col};
+function add_coordinates(
+  coordinate1: Coordinate,
+  coordinate2: Coordinate,
+): Coordinate {
+  return {
+    row: coordinate1.row + coordinate2.row,
+    col: coordinate1.col + coordinate2.col,
+  };
 }
 
 function findChar(split: string[][], char: string): Coordinate[] {
   return split.reduce((accumulator: Coordinate[], current, rowIndex) => {
-    return [...accumulator,
-      ...current.reduce((lineAccumulator: Coordinate[], currentLine, colIndex) => {
-        if (currentLine !== char) {
-          return lineAccumulator;
-        }
-        return [...lineAccumulator, {row: rowIndex, col: colIndex}]
-      }, [])]
-  }, [])
+    return [
+      ...accumulator,
+      ...current.reduce(
+        (lineAccumulator: Coordinate[], currentLine, colIndex) => {
+          if (currentLine !== char) {
+            return lineAccumulator;
+          }
+          return [...lineAccumulator, { row: rowIndex, col: colIndex }];
+        },
+        [],
+      ),
+    ];
+  }, []);
 }
 
 const SEARCH_VECTORS: Coordinate[] = [
-  {row: -1, col: 0},
-  {row: -1, col: 1},
-  {row: 0, col: 1},
-  {row: 1, col: 1},
-  {row: 1, col: 0},
-  {row: 1, col: -1},
-  {row: 0, col: -1},
-  {row: -1, col: -1},
+  { row: -1, col: 0 },
+  { row: -1, col: 1 },
+  { row: 0, col: 1 },
+  { row: 1, col: 1 },
+  { row: 1, col: 0 },
+  { row: 1, col: -1 },
+  { row: 0, col: -1 },
+  { row: -1, col: -1 },
 ];
 
-const LETTERS = ['X', 'M', 'A', 'S'];
+const LETTERS = ["X", "M", "A", "S"];
 
-function search(input: string[][], start_coordinate: Coordinate, vector: Coordinate, letter_index: number): boolean{
-  if (input[start_coordinate.row][start_coordinate.col] !== LETTERS[letter_index]) {
+function search(
+  input: string[][],
+  start_coordinate: Coordinate,
+  vector: Coordinate,
+  letter_index: number,
+): boolean {
+  if (
+    input[start_coordinate.row][start_coordinate.col] !== LETTERS[letter_index]
+  ) {
     return false;
   }
   if (letter_index === LETTERS.length - 1) {
     return true;
   }
-  return search(input, add_coordinates(start_coordinate, vector), vector, letter_index + 1);
+  return search(
+    input,
+    add_coordinates(start_coordinate, vector),
+    vector,
+    letter_index + 1,
+  );
 }
 
 function part1(): number {
   const day4input = Deno.readTextFileSync("input/day4.txt");
-  const _example_input =
-    `MMMSXXMASM
+  const _example_input = `MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -68,7 +90,7 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX`;
   const inputsplit = split(day4input);
-  const xes = findChar(inputsplit, 'X');
+  const xes = findChar(inputsplit, "X");
   return xes.reduce((accumulator, coordinate: Coordinate) => {
     return accumulator + SEARCH_VECTORS.reduce((inneraccumulator, vector) => {
       if (search(inputsplit, coordinate, vector, 0)) {
@@ -80,16 +102,15 @@ MXMXAXMASX`;
 }
 
 const SEARCH_VECTORS_DIAGONAL: Coordinate[] = [
-  {row: -1, col: 1},
-  {row: 1, col: 1},
-  {row: 1, col: -1},
-  {row: -1, col: -1},
+  { row: -1, col: 1 },
+  { row: 1, col: 1 },
+  { row: 1, col: -1 },
+  { row: -1, col: -1 },
 ];
 
 function part2(): number {
   const day4input = Deno.readTextFileSync("input/day4.txt");
-  const _example_input =
-    `MMMSXXMASM
+  const _example_input = `MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -100,18 +121,30 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX`;
   const inputsplit = split(day4input);
-  const xes = findChar(inputsplit, 'M');
-  const coordinates = xes.reduce<Coordinate[]>((accumulator, coordinate: Coordinate) => {
-    return [...accumulator, ...SEARCH_VECTORS_DIAGONAL.reduce<Coordinate[]>((inneraccumulator, vector) => {
-      if (search(inputsplit, coordinate, vector, 1)) {
-        inneraccumulator.push(add_coordinates(coordinate, vector))
-      }
-      return inneraccumulator;
-    }, [])];
-  }, []);
+  const xes = findChar(inputsplit, "M");
+  const coordinates = xes.reduce<Coordinate[]>(
+    (accumulator, coordinate: Coordinate) => {
+      return [
+        ...accumulator,
+        ...SEARCH_VECTORS_DIAGONAL.reduce<Coordinate[]>(
+          (inneraccumulator, vector) => {
+            if (search(inputsplit, coordinate, vector, 1)) {
+              inneraccumulator.push(add_coordinates(coordinate, vector));
+            }
+            return inneraccumulator;
+          },
+          [],
+        ),
+      ];
+    },
+    [],
+  );
   const counter: Map<string, number> = new Map();
   coordinates.forEach((coordinate) => {
-    counter.set(`${coordinate.row},${coordinate.col}`, (counter.get(`${coordinate.row},${coordinate.col}`) ?? 0) + 1);
+    counter.set(
+      `${coordinate.row},${coordinate.col}`,
+      (counter.get(`${coordinate.row},${coordinate.col}`) ?? 0) + 1,
+    );
   });
   let result = 0;
   counter.forEach((value) => {
